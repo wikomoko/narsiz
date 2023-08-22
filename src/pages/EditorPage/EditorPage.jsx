8cf58ae191bdb8d15e6fbcd560211ffc309d291a
wikomoko <mokoindonesia45@gmail.com>
2023-08-22 13:16:42 +0700

    add simple template

--- src/pages/EditorPage/EditorPage.jsx
+++ src/pages/EditorPage/EditorPage.jsx
@@ -14,13 +14,13 @@ export default function EditorPage() {
     const [photo, setPhoto] = useState()
     const [twibon, setTwibon] = useState()
     const [dragging, setDragging] = useState(false)
-    const [reduceOpacity,setReduceOpacity] = useState(false)
-    const [rotation,setRotation] = useState(0)
-    const [rotationUi,setRotationUi] = useState(0)
-    const [flipV,setFlipV] = useState(false)
-    const [flipH,setFlipH] = useState(false)
-    const [mulaiX,setMulaiX] = useState(0)
-    const [mulaiY,setMulaiY] = useState(0)
+    const [reduceOpacity, setReduceOpacity] = useState(false)
+    const [rotation, setRotation] = useState(0)
+    const [rotationUi, setRotationUi] = useState(0)
+    const [flipV, setFlipV] = useState(false)
+    const [flipH, setFlipH] = useState(false)
+    const [mulaiX, setMulaiX] = useState(0)
+    const [mulaiY, setMulaiY] = useState(0)
 
 
     const inputTwibon = useRef()
@@ -35,7 +35,7 @@ export default function EditorPage() {
         setPosx(parseInt(posx) + event.movementX)
         setPosy(parseInt(posy) + event.movementY)
         console.log(parseInt(posx) + event.movementX)
-      
+
     };
     function startDrag() {
         setDragging(true);
@@ -54,11 +54,11 @@ export default function EditorPage() {
     function touchMove(e) {
         let deltaX = e.changedTouches[0].clientX - mulaiX;
         let deltaY = e.changedTouches[0].clientY - mulaiY;
-        setPosx(photoRef.current.offsetLeft+deltaX)
-        setPosy(photoRef.current.offsetTop+deltaY)
+        setPosx(photoRef.current.offsetLeft + deltaX)
+        setPosy(photoRef.current.offsetTop + deltaY)
         setMulaiX(e.changedTouches[0].clientX)
         setMulaiY(e.changedTouches[0].clientY)
-        console.log(photoRef.current.offsetLeft+deltaX )
+        console.log(photoRef.current.offsetLeft + deltaX)
     }
     function changePhotoSize(e) {
         if (e.target.value < 0) {
@@ -110,12 +110,15 @@ export default function EditorPage() {
             reader.readAsDataURL(file);
         }
     }
+    function selectTwibonTemplate(e) {
+        setTwibon(e)
+    }
     function downloadResult() {
         DomToImage.toBlob(downloadArea.current).then((blob) => window.saveAs(blob, 'twibonAku.png'))
     }
     function changeRotation(e) {
         setRotationUi(e.target.value)
-        setRotation(e.target.value*2)
+        setRotation(e.target.value * 2)
     }
     return (
         <>
@@ -127,10 +130,10 @@ export default function EditorPage() {
                         <div className="col-lg-4 "  ></div>
                         <div className="col-lg-4">
                             <div className="container py-5 ">
-                                <div id="nih" ref={downloadArea} style={{touchAction:'none'}}> 
+                                <div id="nih" ref={downloadArea} style={{ touchAction: 'none' }}>
                                     <div className="pad" ref={pad}>
                                         <div className="padder" id="padder" draggable="true">
-                                            <img alt="" className="cardx" id="card" src={photo} ref={photoRef} style={{ transform: `scale(${flipV ? '-' : ''}${siz},${flipH ? '-' : ''}${siz})`, left: `${posx}px`, top: `${posy}px`,rotate:`${rotation}deg` }}  />
+                                            <img alt="" className="cardx" id="card" src={photo} ref={photoRef} style={{ transform: `scale(${flipV ? '-' : ''}${siz},${flipH ? '-' : ''}${siz})`, left: `${posx}px`, top: `${posy}px`, rotate: `${rotation}deg` }} />
                                         </div>
                                         <img alt="" className={reduceOpacity ? "fore img-fluid opacity-25" : "fore img-fluid"} id="fore" src={twibon} />
                                         <div className="fore bg-transparent w-100 h-100 " onMouseDown={startDrag} onMouseMove={onDrag} onMouseUp={endDrag} onTouchStart={onTouch} onTouchMove={touchMove}></div>
@@ -140,7 +143,8 @@ export default function EditorPage() {
                                 <div className=" mt-3">
                                     <div className="row">
                                         <div className="col-6 text-md-start">
-                                            <button className="btn w-100 btn-info bg-color-main border border-0 " onClick={triggerInputTwibbon}>Pilih Twibon</button>
+                                            {/* <button className="btn w-100 btn-info bg-color-main border border-0 " onClick={triggerInputTwibbon}>Pilih Twibon</button> */}
+                                            <button className="btn w-100 btn-info bg-color-main border border-0 " data-bs-toggle="modal" data-bs-target="#exampleModal">Pilih Twibon</button>
                                         </div>
                                         <input type='file' ref={inputTwibon} onChange={(e) => showTwibon(e)} id="bac" className="d-none" />
                                         <input type='file' ref={inputPhoto} onChange={(e) => showPhoto(e)} id="pilih" className="d-none" />
@@ -170,10 +174,10 @@ export default function EditorPage() {
                                                     <input type="range" min="-100" max="100" value={posyUi} className="form-range" id="posy" onChange={(e) => changePositionVertical(e)} />
                                                     <div className="row">
                                                         <div className="col-6">
-                                                            <button className="btn btn-info w-100" onClick={()=>setFlipV(!flipV)}>Flip V</button>
+                                                            <button className="btn btn-info w-100" onClick={() => setFlipV(!flipV)}>Flip V</button>
                                                         </div>
                                                         <div className="col-6">
-                                                          <button className="btn btn-info w-100" onClick={()=>setFlipH(!flipH)}>Flip H</button>
+                                                            <button className="btn btn-info w-100" onClick={() => setFlipH(!flipH)}>Flip H</button>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -189,6 +193,30 @@ export default function EditorPage() {
                             </div>
                         </div>
                         <div className="col-lg-4 "></div>
+                    </div>
+                </div>
+
+                {/* Modal */}
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">Pilih Template</h1>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <button className="btn btn-info border border-0 bg-color-main w-100" onClick={triggerInputTwibbon} data-bs-dismiss="modal">Upload Template</button>
+                                <p className="text-center mt-3">Atau</p>
+                                <p>Pilih cotoh template berikut ini:</p>
+                                <div className="w-100 btn" onClick={()=>selectTwibonTemplate('/templates/sample1.png')} data-bs-dismiss="modal">
+                                    <img src="/templates/sample1.png" className="img-fluid" alt="" />
+                                </div>
+                                <br />
+                                <div className="w-100 btn" onClick={()=>selectTwibonTemplate('/templates/sample2.png')} data-bs-dismiss="modal">
+                                    <img src="/templates/sample2.png" className="img-fluid" alt="" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
